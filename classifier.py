2c49550b8e9e0f3d3ebd21eb8520c0ed8908fd52
arlen <a870128rlen@gmail.com>
2022-11-10 12:46:39 +0000

    train with pretrained model

--- classifier.py
+++ classifier.py
@@ -2,6 +2,7 @@ import argparse
 from pickletools import optimize
 from sched import scheduler
 import torch
+import torch.nn as nn
 from torchvision import models
 from torchvision import transforms
 from torch.utils.data import DataLoader
@@ -9,12 +10,13 @@ import torch.optim as optim
 
 from classifier_train import Train
 from data import Data_mura
+from models.resnet_simclr import ResNetSimCLR
 
 
 parser = argparse.ArgumentParser(description='PyTorch MURA shoulder fracture classification')
 
 parser.add_argument("--workers", default=2, type=int)
-parser.add_argument("--epochs", default=50, type=int)
+parser.add_argument("--epochs", default=200, type=int)
 parser.add_argument("--batch_size", default=48, type=int)
 parser.add_argument("--lr", default=1e-5, type=float)
 parser.add_argument("--weight_decay", default=1e-4, type=float)
@@ -56,7 +58,21 @@ def main():
         pin_memory=True
     )
 
-    model = models.resnet18(weights=None, num_classes=2)
+    ###
+    # model = models.resnet18(weights="None", num_classes=2)
+    ###
+    model = models.resnet18(weights="ResNet18_Weights.IMAGENET1K_V1")
+    dim_mlp = model.fc.in_features
+    model.fc = nn.Linear(dim_mlp, 2)
+    ###
+    # model = ResNetSimCLR(base_model="resnet18", out_dim=128)
+    # checkpoint = torch.load("/home/arlen/xray_classification/runs/Nov06_05-03-02_tw-ws_self_supervision/checkpoint_0050.pth.tar")
+    # model.load_state_dict(checkpoint["state_dict"])
+    # model = nn.Sequential(
+    #     model,
+    #     nn.Linear(128,2)
+    # )
+
     optimizer = optim.Adam(model.parameters(), args.lr)
     train_process = Train(model=model, optimizer=optimizer, args=args)
     train_process.train(train_loader, val_loader)
